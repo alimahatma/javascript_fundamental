d15362345e648b336ec4478dbf6114a80294bed1
alimahatma <laluallistilo@gmail.com>
2020-10-03 21:28:20 +0700

    latihan membuat variabel

---
+++ app.js
@@ -0,0 +1,29 @@
+var name = "Cak Ali";
+console.log(name);
+
+name =  "Indi"
+console.log(name);
+
+//init variabel
+var griting;
+console.log(griting);
+griting = 'Hello Boy';
+console.log(griting);
+
+//aturan penulisan variabel yg diperbolehkan
+var variabel;
+var _variabel;
+var vari_abel;
+var $variabel;
+var variabel1234;
+var var123abel567;
+
+//multi word
+var firstname;
+var firstName;
+var FirstName;
+first_name;
+
+//tidak diperbolehkan 
+var 12343var;
+var -vari_abel;
